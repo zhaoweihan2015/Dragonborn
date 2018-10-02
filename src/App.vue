d3a18d81b738a5c1291c7d2541a45706766b92dc
Guafree <727095319@qq.com>
2018-10-02 10:49:22 +0800

    换行核心完成
    可算是可以换行啊

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="result">
-        <el-input type="textarea" autosize placeholder="请输入内容" v-model="info">
-        </el-input>
-        <el-row>
-            <el-col :span="24" class="result-show">
-                <canvas ref="canvas" :width="canvaSet.w" :height="canvaSet.h"></canvas>
-            </el-col>
-        </el-row>
-    </div>
+  <div class="result">
+    <el-input type="textarea" autosize placeholder="请输入内容" v-model="info">
+    </el-input>
+    <el-row>
+      <el-col :span="24" class="result-show">
+        <canvas ref="canvas" :width="canvaSet.w" :height="canvaSet.h"></canvas>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -15,8 +15,8 @@ export default {
   data() {
     return {
       canvaSet: {
-        w: 320,
-        h: 55,
+        w: 800,
+        h: 600,
         fontColor: "#fff",
         backGroundColor: "#000"
       },
@@ -69,29 +69,26 @@ export default {
     printWord(val) {
       this.clear();
       let { w, fontColor } = this.canvaSet;
-      let maxNumber = parseInt((w - 10) / 35);
+      let maxNumber = parseInt((w - 10) / 36);
       this.ctx.fillStyle = fontColor;
 
-      let nowValueList = [""],
-        nowLength = 0,
+      let nowValueList = [],
+        nowLength,
         nowY = 0;
-      val.split(" ").forEach(e => {
-        if (e.length < maxNumber) {
-          console.log(e);
-          if (nowLength + e.length >= maxNumber) {
+      val.split(/[\n|\r]/).forEach(line => {
+        nowLength = 0
+        line.split(/\s/).forEach(e => {
+          if (nowLength + e.length >= (maxNumber - 1)) {
+            nowLength = e.length;
             nowY++;
-            nowLength = 0;
             nowValueList[nowY] = e + " ";
           } else {
             nowLength += e.length;
-            nowValueList[nowY] += e + " ";
+            nowValueList[nowY] = (nowValueList[nowY] ? nowValueList[nowY] : "") + e + " ";
           }
-        } else {
-          nowY++;
-          nowValueList.push(e);
-        }
-      });
-      console.log(nowValueList);
+        });
+        nowY++;
+      })
       nowValueList.forEach((e, i) => {
         this.ctx.fillText(e, 5, 45 * (i + 1));
       });
